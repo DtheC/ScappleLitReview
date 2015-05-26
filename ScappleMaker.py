ed7f91b99a560bec93b28f9bdf0bcb7e321ef33d
Travis <travis@denouncetheclock.com>
2015-05-26 14:15:58 +1000

    Continued to define ScappleXML class

--- ScappleMaker.py
+++ ScappleMaker.py
@@ -1,1 +1,54 @@
 __author__ = 'Travis'
+import xml.etree.ElementTree as ET
+
+class ScappleXML:
+    """A class for holding and modifying the data of a Scapple XML file"""
+    def __init__(self, XMLFile):
+        self.tree = ET.parse(XMLFile)
+        self.root = tree.getroot()
+        #a List of all current titles and their associated nodeID
+        self.allCurrentTitles = {}
+        #The notes connected to each other (regardless of whether it's an arrow or dashed line)
+        self.allCurrentConnections = {}
+        #The notes which are connected by an arrow. NOTE: The note ID is the origin with the list being the notes it points TO
+        self.allCurrentPoints = {}
+        self.highestId = 0
+        self.loadXML()
+
+    def loadXML(self):
+        #Populate variables...
+        for child in self.root[0]:
+            if int(child.attrib.get("ID")) > self.highestId:
+                self.highestId = int(child.attrib.get("ID"))
+            self.allCurrentTitles[child[1].text] = child.attrib.get("ID")
+            #Should check here to make sure the ConnectedNoteIDs exists as below with point...
+            self.allCurrentConnections[child.attrib.get("ID")] = self.populateCurrentConnections(child[2].text)
+            #See if the node is pointing to anything (with the explicit arrow)
+            if child.find("PointsToNoteIDs") is not None:
+                self.allCurrentPoints[child.attrib.get("ID")] = self.populateCurrentConnections(child.find("PointsToNoteIDs").text)
+        #add one to highest to get newest id for new nodes
+        self.highestId += 1
+
+        #Is used to populate both current connections and current points
+    def populateCurrentConnections(self, connections):
+        #list to return
+        returnList = []
+        #first split along commas
+        numbers = connections.split(',')
+        #print l
+        for n in numbers:
+            #for each number, first check if it is hypened
+            if "-" in n:
+                #if there's a hypen then call expand to get a list of those
+                returnList.extend(self.expandNumbers(n))
+            else:
+                returnList.append(int(n))
+        return returnList
+
+        #called if there's a hypen of numbers in the connections.
+    def expandNumbers (self, theString):
+        r = []
+        numbers = theString.split("-")
+        for x in range(int(numbers[0]), int(numbers[1])+1):
+            r[len(r):] = [x]
+        return r
