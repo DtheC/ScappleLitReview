78f1b538e17c06dd5ad5c8add2ee52cd6f80cff0
Travis <travis@denouncetheclock.com>
2015-05-26 15:36:46 +1000

    Defined user input class

--- ScappleMaker.py
+++ ScappleMaker.py
@@ -8,7 +8,9 @@ class ScappleXML:
         self.tree = ET.parse(XMLFile)
         self.root = self.tree.getroot()
 
-        #list of all IDs which exist
+        #list of all IDs which exist.
+        # Should probably just switch all current titles so the key is ID and the title is the value...
+        #But maybe best to leave allCurrentTitles the other way so that I can search by title...
         self.allCurrentIDs = []
         #a dict of all current titles and their associated nodeID
         self.allCurrentTitles = {}
@@ -62,6 +64,7 @@ class ScappleXML:
 
     def createTitle(self, name):
         self.allCurrentTitles[name] = self.highestId
+        self.allCurrentIDs.append(self.highestId)
         self.highestId += 1
 
     #For checking if title exists and setting current id and title to new values
@@ -77,36 +80,54 @@ class ScappleXML:
             self.activeId = self.allCurrentTitles[name]
             print ("Now adding reference to existing title: {} (ID: {})").format(self.activeTitle, self.activeId)
 
+    def printTitles(self):
+        for k in self.allCurrentTitles:
+            print ("({}) {}").format(self.allCurrentTitles[k], k)
 
 class InputController:
     """A controller for making things happen from user input."""
     def __init__(self):
         self.XMLFile = sys.argv[1]
-        self.lit = ScappleXML(XMLFile)
+        self.lit = ScappleXML(self.XMLFile)
         self.userinput = raw_input('Title to add references to (type exit to quit, type list to choose from existing titles): ')
 
     def nextaction(self, input):
-        if input.strip() = 'exit'
+        if input == 'exit':
             self.userinput = 'exit'
-        if input.strip() = 'list'
+        elif input == 'list':
             self.listCurrentTitles()
+            self.num = raw_input('Type ID number to activate text: ')
+            if int(self.num) in self.lit.allCurrentIDs:
+                for title, id in self.lit.allCurrentTitles:
+                    if id == int(self.num):
+                        self.lit.activeTitle = title
+                self.lit.activeId = int(self.num)
+            else:
+                "Couldn't find that title, sorry. Check your ID was correct."
+        else:
+            print "Sorry, didn't understand that command..."
 
     def listCurrentTitles(self):
-
+        self.lit.printTitles()
 
 
 control = InputController()
 
 while control.userinput.strip() != 'exit':
-    #ask which title we should work from
-    userinput = raw_input('Title to add references to (type exit to quit, type list to choose from existing titles): ')
+    print ("Current title: {}").format(control.lit.activeTitle)
+    control.nextaction(raw_input('Title to add references to (type exit to quit, type list to choose from existing titles): '))
 
-    if userinput.strip() is 'list':
-        #print list and ask for ID
-        lit.printList()
-        userinput = raw_input('Type ID number to activate text')
+    # ask which title we should work from
+    #userinput = raw_input('Title to add references to (type exit to quit, type list to choose from existing titles): ')
 
 
 
+#    if userinput.strip() is 'list':
+#        #print list and ask for ID
+#        lit.printList()
+#        userinput = raw_input('Type ID number to activate text')
 
-    lit.checkIfTitleExists(userinput)
+
+
+
+    #lit.checkIfTitleExists(userinput)
